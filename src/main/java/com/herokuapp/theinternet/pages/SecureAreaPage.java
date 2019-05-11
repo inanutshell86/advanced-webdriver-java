98c8a5526b0b70aefaa394aedb6085cff0ea2131
Ihor Khrystiuk <inanutshell86@gmail.com>
2019-05-11 20:10:19 +0300

    add page objects for pages

---
+++ src/main/java/com/herokuapp/theinternet/pages/SecureAreaPage.java
@@ -0,0 +1,33 @@
+package com.herokuapp.theinternet.pages;
+
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class SecureAreaPage extends BasePageObject {
+	
+	private String pageUrl = "http://the-internet.herokuapp.com/secure";
+	
+	private By logoutBtn = By.xpath("//a[@class='button secondary radius']");
+	private By msg = By.id("flash");
+
+	public SecureAreaPage(WebDriver driver, Logger log) {
+		super(driver, log);
+	}
+
+	/** Get URL variable from PageObject */
+	protected String getPageUrl() {
+		return pageUrl;
+	}
+	
+	/** Verification if logOutButton is visible on the page */
+	protected boolean isLogOutBtnVisible( ) {
+		return find(logoutBtn).isDisplayed();
+	}
+	
+	/** Return text from success message */
+	protected String getSuccessMessageText() {
+		return find(msg).getText();
+	}
+
+}
